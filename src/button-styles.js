e9705719d0d7bfdc5081a1bb9de8e54e88c52417
Dejan Vjestica <dejan.vjestica@aperto.com>
2019-01-15 10:08:30 +0100

    Add Modules

---
+++ src/button-styles.js
@@ -0,0 +1,10 @@
+const red = "color: red;";
+const blue = "color: blue;";
+const makeColorStyle = (color) => {
+	return `color: ${color};`
+	console.log(color);
+};
+
+exports.red = red;
+exports.blue = blue;
+exports.makeColorStyle = makeColorStyle;
---
+++ src/footer.js
@@ -0,0 +1,8 @@
+const top = "top";
+const bottom = "bottom";
+
+// export default () => {
+// 	return "footer"
+// };
+
+export { top, bottom};
